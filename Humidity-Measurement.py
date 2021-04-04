44b3ba4124e30d6406e30d7d28517bec4e820a4d
Georgi Tsonkov <gtsonkov@abv.bg>
2021-04-04 08:32:52 +0200

    -edit

--- Humidity-Measurement.py
+++ Humidity-Measurement.py
@@ -1,45 +1,45 @@
-# Simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Tony DiCola
-# License: Public Domain
 import time
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
-import Adafruit_GPIO.SPI as SPI
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpo
 import Adafruit_MCP3008
 
 # Software SPI configuration:
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(26,GPIO.OUT)
-GPIO.output(26,GPIO.LOW)
 CLK  = 18
 MISO = 23
 MOSI = 24
 CS   = 25
+out = 26
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
-# Hardware SPI configuration:
-#SPI_PORT   = 0
-#SPI_DEVICE = 0
-#mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-
-print('Reading MCP3008 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
-# Main program loop.
-while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*8
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
-    # Print the ADC values.
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-    # Pause for half a second.
-    GPIO.output(26,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(26,GPIO.LOW)
-    time.sleep(0.5)
+gpo.setup(out,gpo.OUT)
+gpo.output(out,gpo.LOW)
     
+try:
+    # Hardware SPI configuration:
+    #SPI_PORT   = 0
+    #SPI_DEVICE = 0
+    #mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+    
+    
+    print('Reading MCP3008 values, press Ctrl-C to quit...')
+    # Print nice channel column headers.
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+    print('-' * 57)
+    # Main program loop.
+    while True:
+        # Read all the ADC channel values in a list.
+        gpo.output(out,gpo.LOW)
+        values = [0]*8
+        for i in range(8):
+            # The read_adc function will get the value of the specified channel (0-7).
+            values[i] = mcp.read_adc(i)
+        # Print the ADC values.
+        print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+        # Pause for half a second.
+        gpo.output(out,gpo.HIGH)
+        time.sleep(0.5)
+        gpo.output(out,gpo.LOW)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+        gpo.output(out,gpo.LOW)
+        quit()
